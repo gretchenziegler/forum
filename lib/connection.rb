63efe638c918925a7033a90281d747c3826a84f1
gretchenziegler <gretchenziegler@gmail.com>
2015-01-06 10:24:15 -0500

    created method for unsubscribing

--- lib/connection.rb
+++ lib/connection.rb
@@ -1,12 +1,12 @@
 require "active_record"
 
-# ActiveRecord::Base.establish_connection('postgresql://' + ENV["DB_INFO"] + '@127.0.0.1/forum')
+ActiveRecord::Base.establish_connection('postgresql://' + ENV["DB_INFO"] + '@127.0.0.1/forum')
 
-ActiveRecord::Base.establish_connection({
-	:adapter => "postgresql",
-	:host => "localhost",
-	:username => "gretchenziegler",
-	:database => "forum"
-})
+# ActiveRecord::Base.establish_connection({
+# 	:adapter => "postgresql",
+# 	:host => "localhost",
+# 	:username => "gretchenziegler",
+# 	:database => "forum"
+# })
 
 ActiveRecord::Base.logger = Logger.new(STDOUT)
